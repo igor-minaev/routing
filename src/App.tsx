4127b5d81d3466f71a7f6bd1c8c7d6d7639dd353
Igor Minayeu <igor.minayeu@gmail.com>
2026-06-06 19:57:57 +0300

    add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {Puma} from "./components/Puma";
 import {Reebok} from "./components/Reebok";
 import {Asics} from "./components/Asics";
 import {Error404} from "./components/Error404";
+import styled from "styled-components";
 
 function App() {
 
@@ -14,18 +15,30 @@ function App() {
             <header className={styles.header}>SHOES</header>
             <div className={styles.body}>
                 <nav className={styles.nav}>
-                    <div>
-                        <NavLink className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink} to='/page1'>Adidas</NavLink>
-                    </div>
-                    <div>
-                        <NavLink className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink} to='/page2'>Puma</NavLink>
-                    </div>
-                    <div>
-                        <NavLink className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink} to='/page3'>Reebok</NavLink>
-                    </div>
-                    <div>
-                        <NavLink className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink} to='/page4'>Asics</NavLink>
-                    </div>
+                    {/*<div>*/}
+                    {/*    <NavLink className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink} to='/page1'>Adidas</NavLink>*/}
+                    {/*</div>*/}
+                    {/*<div>*/}
+                    {/*    <NavLink className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink} to='/page2'>Puma</NavLink>*/}
+                    {/*</div>*/}
+                    {/*<div>*/}
+                    {/*    <NavLink className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink} to='/page3'>Reebok</NavLink>*/}
+                    {/*</div>*/}
+                    {/*<div>*/}
+                    {/*    <NavLink className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink} to='/page4'>Asics</NavLink>*/}
+                    {/*</div>*/}
+                    <NavWrapper>
+                        <NavLink to='/page1'>Adidas</NavLink>
+                    </NavWrapper>
+                    <NavWrapper>
+                        <NavLink to='/page2'>Puma</NavLink>
+                    </NavWrapper>
+                    <NavWrapper>
+                        <NavLink to='/page3'>Reebok</NavLink>
+                    </NavWrapper>
+                    <NavWrapper>
+                        <NavLink to='/page4'>Asics</NavLink>
+                    </NavWrapper>
                 </nav>
                 <div className={styles.content}>
                     <Routes>
@@ -43,5 +56,24 @@ function App() {
         </>
     )
 }
+
+const NavWrapper = styled.div`
+    margin-left: 10px;
+    font-size: 2rem;
+
+    & > a {
+        text-decoration: none;
+        color: #2c4939;;
+    }
+
+    & > a.active {
+        text-decoration: underline;
+        color: #45c17e;
+    }
+
+    & > a:hover {
+        color: #048552;
+    }
+`
 
 export default App
